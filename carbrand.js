c66c9accc49b258b30179f0c17403252d1862b2d
Johnstri Sinulingga <johanstri666@gmail.com>
2025-10-31 18:36:29 +0700

    Add files via upload

---
+++ carbrand.js
@@ -0,0 +1,27 @@
+const carBrands = {
+  Audi: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Audi-Logo_2016.svg/1200px-Audi-Logo_2016.svg.png",
+  Arash: "/asset/carlogo/Arash-Logo-500x281.png",
+  "Aston Martin":
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Aston_Martin_Lagonda_logo.svg/1200px-Aston_Martin_Lagonda_logo.svg.png",
+  Acura:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Acura_logo.svg/1200px-Acura_logo.svg.png",
+  BMW: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/1200px-BMW.svg.png",
+  Ferrari:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Ferrari_Logo.svg/1200px-Ferrari_Logo.svg.png",
+  Ford: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Ford_Motor_Company_Logo.svg/1200px-Ford_Motor_Company_Logo.svg.png",
+  Honda: "asset/carLogo/honda.png",
+  Jeep: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Jeep_logo.svg/1200px-Jeep_logo.svg.png",
+  Koenigsegg:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Koenigsegg_logo.svg/1200px-Koenigsegg_logo.svg.png",
+  Lamborghini: "/asset/carlogo/Lamborghini.svg",
+  Maserati:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Maserati_logo.svg/1200px-Maserati_logo.svg.png",
+  Mazda: "asset/carLogo/mazda.png",
+  McLaren: "/asset/carlogo/McLaren.png",
+  "Mercedes-Benz":
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Mercedes-Benz_Logo_2010.svg/1200px-Mercedes-Benz_Logo_2010.svg.png",
+  Mitsubishi:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Mitsubishi_logo.svg/1200px-Mitsubishi_logo.svg.png",
+  BYD: "asset/carLogo/byd.svg",
+  WMotors: "asset/carLogo/wmotors.png",
+};
